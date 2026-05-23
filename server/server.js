1f1fdda1060305d692c19bf60a087f64d2eb1fbf
Nandini Sihare <168717179+nandinisihare2332@users.noreply.github.com>
2026-05-24 01:11:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins=['http://localhost:5174']
+const allowedOrigins=['https://navridhi-2.onrender.com']
 
 app.use(express.json());
 app.use(cookieParser());
